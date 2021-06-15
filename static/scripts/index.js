a2c184947efbfca544ff3965fab0475a65bfeebc
Maria <maria.bn.nguyen@gmail.com>
2021-06-15 20:36:57 +1000

    add logo asset

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,9 +1,16 @@
 function _doMail(e) {
-    const name = document.querySelector('[name="your-name"]').value;
-    const email = document.querySelector('[name="your-email"]').value;
-    const mobile = document.querySelector('[name="mobile"]').value;
-    const company = document.querySelector('[name="company"]').value;
-    const address = document.querySelector('[name="your-message"]').value;
+    const fields = {
+        name: document.querySelector('[name="your-name"]'),
+        email: document.querySelector('[name="your-email"]'),
+        mobile: document.querySelector('[name="mobile"]'),
+        company: document.querySelector('[name="company"]'),
+        address: document.querySelector('[name="your-message"]')
+    };
+    const name = fields.name.value;
+    const email = fields.email.value;
+    const mobile = fields.mobile.value;
+    const company = fields.company.value;
+    const address = fields.address.value;
     const body = `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nCompany: ${company}\nAddress: ${address}`;
     location.href = `mailto:thong@ttinsurance.com.au?subject=Website%20Enquiry&body=${encodeURI(body)}`;
 }
