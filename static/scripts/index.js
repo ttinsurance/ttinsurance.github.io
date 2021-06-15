ce8f4e441762e9c54611cf8af91e4563935311a3
Maria <maria.bn.nguyen@gmail.com>
2021-06-15 22:19:49 +1000

    add validation

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,4 +1,5 @@
 function _doMail(e) {
+    const REQUIRED_FIELDS = ['name', 'email', 'mobile'];
     const fields = {
         name: document.querySelector('[name="your-name"]'),
         email: document.querySelector('[name="your-email"]'),
@@ -6,11 +7,27 @@ function _doMail(e) {
         company: document.querySelector('[name="company"]'),
         address: document.querySelector('[name="your-message"]')
     };
+
     const name = fields.name.value;
     const email = fields.email.value;
     const mobile = fields.mobile.value;
     const company = fields.company.value;
     const address = fields.address.value;
+
+    let invalidFields = false;
+    REQUIRED_FIELDS.forEach(field => {
+        if (!fields[field].value) {
+            fields[field].classList.add('invalid');
+            invalidFields = true;
+        } else {
+            fields[field].classList.remove('invalid');
+        }
+    });
+    
+    if (invalidFields) {
+        return;
+    }
+    
     const body = `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nCompany: ${company}\nAddress: ${address}`;
     location.href = `mailto:thong@ttinsurance.com.au?subject=Website%20Enquiry&body=${encodeURI(body)}`;
 }
