7b2cd6085648ad568ec49d727504b967ac2f278f
Maria <maria.bn.nguyen@gmail.com>
2021-08-30 21:38:35 +1000

    rebuild site

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,11 +1,12 @@
 function _doMail(e) {
-    const REQUIRED_FIELDS = ['name', 'email', 'mobile', 'insurance'];
+    const REQUIRED_FIELDS = ['name', 'email', 'mobile'];
     const fields = {
         name: document.querySelector('[name="your-name"]'),
         email: document.querySelector('[name="your-email"]'),
         mobile: document.querySelector('[name="mobile"]'),
         company: document.querySelector('[name="company"]'),
-        insurance: document.querySelector('[name="insurance"]')
+        insurance: document.querySelector('[name="insurance"]'),
+        info: document.querySelector('[name="additional-info"]')
     };
 
     const name = fields.name.value;
@@ -13,6 +14,7 @@ function _doMail(e) {
     const mobile = fields.mobile.value;
     const company = fields.company.value;
     const insurance = fields.insurance.value;
+    const info = fields.info.value;
 
     let invalidFields = false;
     REQUIRED_FIELDS.forEach(field => {
@@ -28,6 +30,6 @@ function _doMail(e) {
         return;
     }
     
-    const body = `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nCompany: ${company}\nInsurance required: ${insurance}`;
+    const body = `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nCompany: ${company}\nInsurance required: ${insurance}\nAdditional Info: ${info}`;
     location.href = `mailto:thong@ttinsurance.com.au?subject=Website%20Enquiry&body=${encodeURI(body)}`;
 }
